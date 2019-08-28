8b76c5d4b74bb36a84201bf1324be57d675fdca9
lixu <lixu@jingantech.com>
2019-08-28 21:36:02 +0800

    lixu2第一次提交

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.RequestWrapper;
+
+@RestController
+public class LoginController {
+    @RequestMapping("/login")
+    public String login(){
+        return "hello";
+    }
+}
